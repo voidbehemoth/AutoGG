09548bc659adad44677d6d8f270439933b99c216
VoidBehemoth <VoidBehemoth@gmail.com>
2023-09-14 21:21:19 -0600

    Made it all work... finally.

--- AutoGG/src/AutoGG.cs
+++ AutoGG/src/AutoGG.cs
@@ -1,30 +1,53 @@
-﻿
-
-using System;
-using System.IO;
-using SML;
-using Game.Interface;
+﻿using Game.Interface;
+using Game.Services;
+using Game.Simulation;
 using HarmonyLib;
+using Server.Shared.Info;
 using Server.Shared.State;
-
+using Services;
+using SML;
+using static Utils.Storage;
 
 namespace AutoGG
 {
-
-
-    /*
-    [HarmonyPatch(typeof(HudEndGame), "HandleGameResults")]
-    public class AutoGG
+    public static class GameEndMessage
     {
-        [HarmonyPostfix]
-        public static void PostPhaseFix(HudEndGame __instance, GameResults results)
+
+        private static GameResults results;
+        public static void HandleGameResults(GameResults gameResults)
         {
             if (results.entries.Count >= 1)
             {
-                Services.Service.Game.Sim.simulation.SendChat("gg");
+                results = gameResults;
             }
         }
-    }*/
+
+        public static void HandleGameInfoChanged(GameInfo gameInfo)
+        {
+            if (results == null) return;
+
+            if (gameInfo.gamePhase == GamePhase.RESULTS && ModSettings.GetBool("Send Game Over Message"))
+            {
+                Service.Game.Sim.simulation.SendChat(AutoGGUtils.GetGameOverMessage(results));
+                results = null;
+            }
+        }
+
+
+    }
+
+    [HarmonyPatch(typeof(PickNamesPanel), "HandleSubmitName")]
+    public class AutoGG
+    {
+        [HarmonyPostfix]
+        public static void PostPhasefix(PickNamesPanel __instance, string name)
+        {
+            if (ModSettings.GetBool("Send Game Start Message"))
+            {
+                Service.Game.Sim.simulation.SendChat(AutoGGUtils.GetGameStartMessage());
+            }
+        }
+    }
 
     public static class MyPluginInfo
     {
@@ -33,6 +56,43 @@ namespace AutoGG
         public const string PLUGIN_NAME = "AutoGG";
 
         public const string PLUGIN_VERSION = "1.0.0";
+    }
+
+    public static class AutoGGUtils
+    {
+        
+
+        public static string GetGameOverMessage(GameResults results)
+        {
+            GameSimulation sim = Services.Service.Game.Sim.simulation;
+
+            string wonMessage = ModSettings.GetString("Won Game Message");
+            string lostMessage = ModSettings.GetString("Lost Game Message");
+            string endMessage = ModSettings.GetString("Game Over Message");
+
+            bool won = results.entries.Find(entry => entry.playerId.Equals(Services.Service.Home.UserService.UserInfo.AccountID)).won;
+            if (!string.IsNullOrEmpty(wonMessage) && won)
+            {
+                return wonMessage;
+            } else if (!string.IsNullOrEmpty(lostMessage) && !won)
+            {
+                return lostMessage;
+            } else if (!string.IsNullOrEmpty(endMessage)) {
+                return endMessage;
+            } else return l10n("DEFAULT_GAME_END");
+        }
+
+        public static string GetGameStartMessage()
+        {
+            string startMessage = ModSettings.GetString("Game Start Message");
+
+            return (string.IsNullOrEmpty(startMessage)) ? l10n("DEFAULT_START_GAME") : startMessage;
+        }
+
+        public static string l10n(string key)
+        {
+            return Service.Home.LocalizationService.GetLocalizedString(key);
+        }
     }
 
 }
---
+++ AutoGG/src/SalemModLoader.cs
@@ -0,0 +1,41 @@
+﻿using AutoGG;
+using HarmonyLib;
+using Home.Services;
+using Server.Shared.Info;
+using Server.Shared.State;
+using Services;
+using SML;
+
+
+namespace SalemModLoader
+{
+
+    [Mod.SalemMod]
+    public class SalemModLoader
+    {
+        public static void Start()
+        {
+            GameInfoObservation gameInfo = Service.Game.Sim.info.gameInfo;
+            gameInfo.OnDataChanged = (Action<GameInfo>)Delegate.Combine(gameInfo.OnDataChanged, new Action<GameInfo>(GameEndMessage.HandleGameInfoChanged));
+            StateProperty<GameResults> gameResults = Service.Game.Sim.simulation.gameResults;
+            gameResults.OnChanged = (Action<GameResults>)Delegate.Combine(gameResults.OnChanged, new Action<GameResults>(GameEndMessage.HandleGameResults));
+
+
+            Console.WriteLine("ain't no way");
+        }
+    }
+
+    [HarmonyPatch(typeof(HomeLocalizationService), "RebuildStringTables")]
+    public class LocalizationKeys
+    {
+        // Token: 0x06000013 RID: 19 RVA: 0x000027A0 File Offset: 0x000009A0
+        [HarmonyPostfix]
+        public static void Postfix(HomeLocalizationService __instance)
+        {
+            Dictionary<string, string> stringTable_ = Traverse.Create<HomeLocalizationService>().Property("stringTable_").GetValue<Dictionary<string, string>>();
+            stringTable_.Add("DEFAULT_GAME_END", "gg");
+            stringTable_.Add("DEFAULT_GAME_START", "gl");
+            Traverse.Create<HomeLocalizationService>().Property("stringTable_").SetValue(stringTable_);
+        }
+    }
+}
